277371413fc10c7cd73bf14a3a18004dce3018b6
kimdohoon2 <88535144+kimdohoon2@users.noreply.github.com>
2025-05-06 18:12:20 +0900

    feat : 푸터 컴퍼넌트 반응형까지 완성

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styles from "@/app/components/Footer/Footer.module.scss";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p className={styles.copyright}>
+        &copy; 2025 Kim Do Hoon. All rights reserved. Design & Development by
+        Kim Do Hoon.
+      </p>
+    </footer>
+  );
+}
